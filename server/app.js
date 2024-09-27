486c2be2e8033064708426a617db9ce18ea8c916
Andrey Veselov <lifeforglory@gmail.com>
2024-09-27 11:33:35 +0300

    server is working

---
+++ server/app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const indexRouter = require("./routes/index.routes");
+const serverConfig = require("./config/serverConfig");
+
+serverConfig(app);
+app.use("/api", indexRouter);
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Флэшкардс работает на ${process.env.PORT}  порту ! `);
+});
---
+++ server/routes/index.routes.js
@@ -0,0 +1,11 @@
+const indexRouter = require("express").Router();
+
+// const categoryRouter = require("./postCategories.routes");
+// const authRouter = require("./auth.routes");
+// const postRouter = require("./posts.routes")
+
+// indexRouter.use("/categories", categoryRouter);
+// indexRouter.use("/auth", authRouter);
+// indexRouter.use("/post", postRouter);
+
+module.exports = indexRouter;
