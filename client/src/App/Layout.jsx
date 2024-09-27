ac12e7ce7c357fd9245d75629330c93546359fdb
Andrey Veselov <lifeforglory@gmail.com>
2024-09-27 16:31:43 +0300

    style: addedStyles

--- client/src/App/Layout.jsx
+++ client/src/App/Layout.jsx
@@ -5,14 +5,14 @@ function Layout({user}) {
   return (
     <>
     <nav>
-        <div style={{fontSize: "30px"}}>
-            <ul style={{display: "flex", gap: "20px", listStyle: "none"}}>
+        <div style={{fontSize: "40px", fontWeight: "700"}}> Привет Эльбрусовец ! 
+            <ul style={{display: "flex", justifyContent: "space-between", gap: "20px", listStyle: "none", }}>
                 
                     {
-                        !user &&  (<><li><Link to={"/login"}>Авторизация</Link>
+                        !user &&  (<><li><Link to={"/login"}>Авторизация ? </Link>
             </li>
             <li>
-              <Link to={"/reg"}>Регистрация</Link>
+              <Link to={"/reg"}>Регистрация : ""</Link>
             </li></>)
                     }
              
@@ -22,7 +22,7 @@ function Layout({user}) {
                 user ? "Привет, " + user.name : null
               }
             </li></ul>
-        <ul style={{display: "flex", gap: "20px", listStyle: "none"}}>
+        <ul style={{display: "flex", flexDirection: "column", gap: "20px", listStyle: "none"}}>
         <li>
               <Link to={"/"}></Link>
             </li>
